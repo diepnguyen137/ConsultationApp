bfdc0c1bb9d1fde74c02d8cc228d376601083a2a
Thien Ho Phu <thien.hophu@gmail.com>
2018-01-03 13:28:46 +0700

    Firebase Write and Read: Basic

--- ConsultationApp/ConsultationApp/ViewController.swift
+++ ConsultationApp/ConsultationApp/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var label: UILabel!
+    
+    
+    var ref : DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = Database.database().reference()
+        
+        // Write into Firebase
+//        self.ref.child("Thien").setValue(["Test": "OK"])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Read from Firebase
+        ref.child("Thien").observe(DataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            let data = value?["Test"] as? String ?? ""
+            print("Firebase: Output: " + data)
+            self.label.text = data
+        }) { (error: Error) in
+            debugPrint("Firebase: Error: " + error.localizedDescription)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
