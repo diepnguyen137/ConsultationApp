aa0ca4aeef9e4ecbd146c1bcef018ae248f909d4
Thien Ho Phu <thien.hophu@gmail.com>
2018-01-12 17:44:07 +0700

    Fix Unsupport Configuration, Non update Post.Storyboard

---
+++ ConsultationApp/ConsultationApp/Controller/PostController.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionTableViewController.swift
+//  Sth2Delete
+//
+//  Created by Tony Tom on 1/12/18.
+//  Copyright © 2018 Tran Trong Tri. All rights reserved.
+//
+
+import UIKit
+
+class PostController: UITableViewController {
+
+    //MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
